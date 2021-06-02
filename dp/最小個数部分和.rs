6f633b4d8e38db22322b697d980f999ad184bb1d
magurofly <54321436+magurofly@users.noreply.github.com>
2021-06-02 16:45:59 +0900

    Update 最小個数部分和.rs

--- dp/最小個数部分和.rs
+++ dp/最小個数部分和.rs
@@ -1,3 +1,7 @@
+// 01-最小個数部分和を計算する
+// 計算量: 時間 O(|A|ΣA), 空間 O(ΣA)
+// @param A 非負整数の集合
+// @param INF 無限とみなす値
 fn subset_sum_min_count(A: &Vec<usize>, INF: usize) -> Vec<Option<usize>> {
   let sum = A.iter().sum::<usize>();
   let mut dp = vec![INF; sum];
