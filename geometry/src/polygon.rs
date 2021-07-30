c9f37b902a1e54020a1aa428c7134686ca398738
magurofly <fishfishfish@hash.fyi>
2021-07-30 23:30:49 +0900

    little updates

--- geometry/src/polygon.rs
+++ geometry/src/polygon.rs
@@ -3,37 +3,28 @@ use num_traits::*;
 
 /// 格子多角形の面積を計算する
 /// - `pos`: 頂点の座標（時計回りまたは反時計回りのどちらか）
-pub fn polygon_area<T: Num + PartialOrd + Clone>(pos: &[impl Pos2D<T>]) -> T {
+pub fn polygon_area<T: Copy + Num + PartialOrd>(pos: &[impl Pos2D<T>]) -> T {
     area(pos).0 / (T::one() + T::one())
 }
 
 /// 格子多角形の座標の並びが時計回りか反時計回りか判定する
 /// - `pos`: 頂点の座標（時計回りまたは反時計回りのどちらか）
-pub fn is_clockwise<T: Num + PartialOrd + Clone>(pos: &[impl Pos2D<T>]) -> bool {
+pub fn is_clockwise<T: Copy + Num + PartialOrd>(pos: &[impl Pos2D<T>]) -> bool {
     area(pos).1
 }
 
-fn area<T: Num + PartialOrd + Clone>(pos: &[impl Pos2D<T>]) -> (T, bool) {
+fn area<T: Copy + Num + PartialOrd>(pos: &[impl Pos2D<T>]) -> (T, bool) {
     let mut area = T::zero();
-    let mut sign = false;
+    // let mut sign = 0;
     for i in 0..pos.len() {
         let p = &pos[i];
         let q = &pos[(i + 1) % pos.len()];
-        let a = p.x() * q.y();
-        let b = p.y() * q.x();
-        if sign {
-            area = area + b;
-            if area < a {
-                area = a - area;
-                sign = false;
-            }
-        } else {
-            area = area + a;
-            if area < b {
-                area = b - area;
-                sign = true;
-            }
-        }
+        area = area + p.x() * q.y() - p.y() * q.x();
+        // area = area + x[sign];
+        // if area < x[sign ^ 1] {
+            // area = x[sign ^ 1] - area;
+            // sign ^= 1;
+        // }
     }
-    (area, sign)
+    (area, area < T::zero())
 }
--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,6 +1,8 @@
 use std::*;
 use collections::*;
 
+pub fn say(x: impl fmt::Display) { println!("{}", x); }
+
 pub fn yesno(c: bool) { println!("{}", if c { "Yes" } else { "No" }); }
 pub fn yes() { yesno(true); }
 pub fn no() { yesno(false); }
@@ -8,6 +10,8 @@ pub fn no() { yesno(false); }
 pub trait MyItertools : Iterator {
   fn to_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect::<Vec<_>>() }
   fn to_vec_reversed(self) -> Vec<Self::Item> where Self: Sized { let mut v = self.collect::<Vec<_>>(); v.reverse(); v }
+  fn to_vec_of<T: From<Self::Item>>(self) -> Vec<T> where Self: Sized { self.map(|x| x.into()).to_vec() }
+  fn try_to_vec_of<T: std::convert::TryFrom<Self::Item>>(self) -> Result<Vec<T>, T::Error> where Self: Sized { let mut xs = Vec::with_capacity(self.size_hint().0); for x in self { xs.push(T::try_from(x)?); } Ok(xs) }
   fn tally(self) -> HashMap<Self::Item, usize> where Self: Sized, Self::Item: Copy + Eq + hash::Hash { let mut counts = HashMap::new(); self.for_each(|item| *counts.entry(item).or_default() += 1 ); counts }
   // fn cumprod<F: Fn(Self::Item, Self::Item) -> Self::Item>(self, init: Self::Item, f: F) -> CumProd<Self, Self::Item, F> where Self: Sized, Self::Item: Copy { CumProd { prod: init, f, iter: self } }
 }
