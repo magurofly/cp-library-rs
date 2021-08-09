aa56f87380df413454f4f4d042649a2b7e863036
magurofly <fishfishfish@hash.fyi>
2021-08-09 16:00:02 +0900

    graph

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -1,3 +1,8 @@
 pub mod vec_graph;
 
+pub trait Graph<E> {
+  fn n(&self) -> usize;
+  fn m(&self) -> usize;
+}
+
 pub use vec_graph::VecGraph;
--- graph/src/vec_graph.rs
+++ graph/src/vec_graph.rs
@@ -1,32 +1,58 @@
-use std::ops::IndexMut;
+use std::ops::{Deref};
 
-pub trait VecGraph<E>: IndexMut<usize> {
+use super::Graph;
+
+pub trait VecGraph<E, Ed: Edge<E>>: Graph<E> + Deref<Target = [Vec<Ed>]> {
+  fn each_edge_from(&self, from: usize, f: impl FnMut(&Ed));
+  
   fn add_arc(&mut self, from: usize, to: usize, weight: E);
   fn add_edge(&mut self, from: usize, to: usize, weight: E) where E: Clone {
     self.add_arc(from, to, weight.clone());
     self.add_arc(to, from, weight);
   }
 
-  fn add_arcs<Ed: EdgeData<E>>(&mut self, arcs: impl IntoIterator<Item = Ed>) where E: Clone {
+  fn add_arcs<D: EdgeData<E>>(&mut self, arcs: impl IntoIterator<Item = D>) where E: Clone {
     for arc in arcs {
       self.add_arc(arc.from(), arc.to(), arc.weight().clone());
     }
   }
 
-  fn add_edges<Ed: EdgeData<E>>(&mut self, edges: impl IntoIterator<Item = Ed>) where E: Clone {
+  fn add_edges<D: EdgeData<E>>(&mut self, edges: impl IntoIterator<Item = D>) where E: Clone {
     for edge in edges {
       self.add_edge(edge.from(), edge.to(), edge.weight().clone());
     }
   }
 }
 
-impl<E> VecGraph<E> for Vec<Vec<(usize, E)>> {
+impl<E, Ed: Edge<E>> Graph<E> for Vec<Vec<Ed>> {
+  fn n(&self) -> usize {
+    self.len()
+  }
+
+  fn m(&self) -> usize {
+    self.iter().map(|edges| edges.len()).sum()
+  }
+}
+
+impl<E> VecGraph<E, (usize, E)> for Vec<Vec<(usize, E)>> {
+  fn each_edge_from(&self, from: usize, mut f: impl FnMut(&(usize, E))) {
+    for edge in &self[from] {
+      (f)(edge);
+    }
+  }
+
   fn add_arc(&mut self, from: usize, to: usize, weight: E) {
     self[from].push((to, weight));
   }
 }
 
-impl VecGraph<()> for Vec<Vec<usize>> {
+impl VecGraph<(), usize> for Vec<Vec<usize>> {
+  fn each_edge_from(&self, from: usize, mut f: impl FnMut(&usize)) {
+    for edge in &self[from] {
+      (f)(edge);
+    }
+  }
+
   fn add_arc(&mut self, from: usize, to: usize, _weight: ()) {
     self[from].push(to);
   }
