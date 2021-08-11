59d9630f1a63bbf9721e5f1ea1d3707a0ea75217
magurofly <fishfishfish@hash.fyi>
2021-08-11 19:14:58 +0900

    update

--- structs/src/lib.rs
+++ structs/src/lib.rs
@@ -1,2 +1,8 @@
-pub mod heap;
-pub use heap::*;
+pub mod dial_heap;
+pub use dial_heap::*;
+
+pub mod radix_heap;
+pub use radix_heap::*;
+
+pub mod segtrees;
+pub use segtrees::*;
--- template/src/lib.rs
+++ template/src/lib.rs
@@ -6,6 +6,9 @@ pub use compress_coords::*;
 pub mod cumulated;
 pub use cumulated::*;
 
+pub mod string;
+pub use string::*;
+
 pub mod traits;
 pub use traits::*;
 
@@ -40,6 +43,8 @@ impl<T: ?Sized> MyItertools for T where T: Iterator {}
 pub trait MyIntoIter : IntoIterator where Self: Sized {
   fn convert<U, V: FromIterator<U>>(self, f: impl FnMut(Self::Item) -> U) -> V { self.into_iter().map(f).collect() }
   fn implode(self, sep: &str) -> String where Self::Item: fmt::Display { self.into_iter().map(|x| format!("{}", x)).to_vec().join(sep) }
+  fn with_index(self) -> Map<Enumerate<Self::IntoIter>, Box<dyn FnMut((usize, Self::Item)) -> (Self::Item, usize)>> { self.into_iter().enumerate().map(Box::new(|(i, x)| (x, i))) }
+  fn it(&self) -> Self::IntoIter where Self: Clone { self.clone().into_iter() }
 }
 impl<T> MyIntoIter for T where T: IntoIterator {}
 
@@ -68,6 +73,92 @@ pub trait MyRangeBounds<T: Copy>: RangeBounds<T> {
   }
   fn end_open_or(&self, default: T) -> T {
     self.end_open().unwrap_or(default)
+  }
+
+  fn lower_bound(&self, mut f: impl FnMut(T) -> bool) -> Option<T> where T: PrimInt {
+    if let Some(mut wa) = self.start_open() {
+      if let Some(mut ac) = self.end_close() {
+        // 二分探索
+        while ac - wa > T::one() {
+          let wj = wa + (ac - wa >> 1);
+          if (f)(wj) {
+            ac = wj;
+          } else {
+            wa = wj;
+          }
+        }
+        if (f)(ac) {
+          Some(ac)
+        } else {
+          None
+        }
+      } else {
+        // 指数探索
+        let mut prev = wa;
+        let mut add = T::one();
+        while !(f)(wa + add) {
+          prev = wa + add;
+          add = add << 1;
+        }
+        (prev .. wa).lower_bound(f)
+      }
+    }  else {
+      if let Some(ac) = self.end_close() {
+        // 指数探索
+        let mut prev = ac;
+        let mut sub = T::one();
+        while (f)(ac - sub) {
+          prev = ac - sub;
+          sub = sub << 1;
+        }
+        (ac - sub ..= prev).lower_bound(f)
+      } else {
+        panic!("No bounds")
+      }
+    }
+  }
+
+  fn upper_bound(&self, mut f: impl FnMut(T) -> bool) -> Option<T> where T: PrimInt {
+    if let Some(mut ac) = self.start_close() {
+      if let Some(mut wa) = self.end_open() {
+        // 二分探索
+        while wa - ac > T::one() {
+          let wj = ac + (wa - ac >> 1);
+          if (f)(wj) {
+            ac = wj;
+          } else {
+            wa = wj;
+          }
+        }
+        if (f)(ac) {
+          Some(ac)
+        } else {
+          None
+        }
+      } else {
+        // 指数探索
+        let mut prev = ac;
+        let mut add = T::one();
+        while (f)(ac + add) {
+          prev = ac + add;
+          add = add << 1;
+        }
+        (prev ..= ac + add).upper_bound(f)
+      }
+    }  else {
+      if let Some(wa) = self.end_open() {
+        // 指数探索
+        let mut prev = wa;
+        let mut sub = T::one();
+        while !(f)(wa - sub) {
+          prev = wa - sub;
+          sub = sub << 1;
+        }
+        (wa - sub .. prev).upper_bound(f)
+      } else {
+        panic!("No bounds")
+      }
+    }
   }
 }
 impl<T: PrimInt, R: RangeBounds<T>> MyRangeBounds<T> for R {
