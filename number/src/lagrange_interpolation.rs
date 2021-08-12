ec3603d350c85102278c54dbb476081366957f4a
magurofly <fishfishfish@hash.fyi>
2021-08-13 03:28:55 +0900

    update

--- number/src/lagrange_interpolation.rs
+++ number/src/lagrange_interpolation.rs
@@ -1,13 +1,12 @@
 use super::*;
 
-pub fn lagrange_interpolation<N: Int>(y: &[N], x: N, m: N) -> N {
+pub fn lagrange_interpolation_mod<N: Int>(y: &[N], x: N, m: N) -> N {
   let n = y.len() - 1;
   if x <= n.cast() {
     return y[x.as_usize()] % m;
   }
   let fact = FactorialInvMod::new(n.cast::<N>(), m);
 
-  let mut ret = N::zero();
   let mut dp = vec![N::one(); n + 1];
   let mut pd = vec![N::one(); n + 1];
   for i in 0 .. n {
@@ -16,6 +15,7 @@ pub fn lagrange_interpolation<N: Int>(y: &[N], x: N, m: N) -> N {
   for i in (1 ..= n).rev() {
     pd[i - 1] = pd[i] * (x - i.cast()) % m;
   }
+  let mut ret = N::zero();
   for i in 0 ..= n {
     let t = y[i] * dp[i] % m * pd[i] % m * fact.fact_inv(i) % m * fact.fact_inv(n - i) % m;
     if (n - i).is_odd() {
