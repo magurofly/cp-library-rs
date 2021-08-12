52a519b000fbd1cdf8cd5be738bec1d7955452f8
magurofly <fishfishfish@hash.fyi>
2021-08-13 01:44:47 +0900

    Add factorial

---
+++ number/src/factorial.rs
@@ -0,0 +1,55 @@
+use std::ops::{Add, Index, Mul};
+
+use super::*;
+
+pub fn factorial<N: Int>(n: N) -> N {
+  if n < N::zero() {
+    panic!("Negative factorial is not supported");
+  }
+
+  let mut r = N::one();
+  let mut i = N::one();
+  while i <= n {
+    r = r * i;
+    i = i.add1();
+  }
+
+  r
+}
+
+#[derive(Debug, Clone)]
+pub struct FactorialMod<N> {
+  fact: Vec<N>,
+}
+impl<N: Int> FactorialMod<N> {
+  pub fn new(n: N, m: N) -> Self {
+    let mut fact = vec![N::one()];
+    let mut r = N::one();
+    let mut i = N::one();
+    while i <= n {
+      r = r * i % m;
+      fact.push(r);
+      i = i.add1();
+    }
+    Self { fact }
+  }
+}
+impl<N: Int> Index<N> for FactorialMod<N> {
+  type Output = N;
+  fn index(&self, idx: N) -> &N {
+    assert!(N::zero() <= idx && idx < self.fact.len().cast());
+    &self.fact[idx.cast::<usize>()]
+  }
+}
+
+pub fn factorials<N: Copy + Add<Output = N> + Mul<Output = N> + PartialOrd>(one: N, n: N) -> Vec<N> {
+  let mut fact = vec![one];
+  let mut r = one;
+  let mut i = one;
+  while i <= n {
+    r = r * i;
+    fact.push(r);
+    i = i + one;
+  }
+  fact
+}
--- number/src/lib.rs
+++ number/src/lib.rs
@@ -4,5 +4,8 @@ pub use int::*;
 pub mod linear_sieve;
 pub use linear_sieve::*;
 
+pub mod factorial;
+pub use factorial::*;
+
 // pub mod modint;
 // pub use modint::*;
