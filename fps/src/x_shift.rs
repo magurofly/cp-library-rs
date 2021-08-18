343e0221733ac024b46772648d473e5d971a8bd5
magurofly <fishfishfish@hash.fyi>
2021-08-19 02:06:32 +0900

    replaced

--- fps/src/x_shift.rs
+++ fps/src/x_shift.rs
@@ -1,26 +1,26 @@
 use super::*;
 use acl_modint::ModIntBase;
 use fft::Convolution;
-use number::*;
+use enumeration::Enumeration;
 
 impl<T: ModIntBase, C: Clone + Convolution<T>> FPS<T, C> where Self: Clone {
   pub fn x_shift(&mut self, a: T) {
     let n = self.deg();
-    let f = FactorialInvMod::new(n, T::modulus() as i64);
+    let f = Enumeration::<T>::new();
     //TODO: replace with thread-local Enumeration struct
     for i in 0 .. n {
-      self[i] *= T::from(f.fact(i));
+      self[i] *= f.fact(i);
     }
     self.reverse();
     let mut g = Self::from(vec![T::from(1); n]);
     for i in 1 .. n {
-      g[i] = g[i - 1] * a * T::from(f.inv(i));
+      g[i] = g[i - 1] * a * f.inv(i);
     }
     *self *= g;
     self.truncate(n);
     self.reverse();
     for i in 0 .. n {
-      self[i] *= T::from(f.fact_inv(i));
+      self[i] *= f.fact_inv(i);
     }
   }
 
