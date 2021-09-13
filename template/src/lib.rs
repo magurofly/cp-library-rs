87ea2d77af959da491e6eea50cec33d472c1452f
magurofly <fishfishfish@hash.fyi>
2021-09-13 22:07:28 +0900

    Merge

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -55,8 +55,6 @@ impl<T> MyIntoIter for T where T: IntoIterator {}
 
 pub trait MySlice<T> : AsRef<[T]> {
   fn citer(&self) -> Cloned<std::slice::Iter<T>> where T: Clone { self.as_ref().iter().cloned() }
-  fn max(&self) -> Option<T> where T: Clone + Ord { self.citer().max() }
-  fn min(&self) -> Option<T> where T: Clone + Ord { self.citer().max() }
   fn sorted(&self) -> Vec<T> where T: Clone + Ord { let mut v = self.as_ref().to_vec(); v.sort(); v }
   fn sorted_by(&self, f: impl FnMut(&T, &T) -> std::cmp::Ordering) -> Vec<T> where T: Clone + Ord { let mut v = self.as_ref().to_vec(); v.sort_by(f); v }
   fn sorted_by_key<U: Ord>(&self, f: impl FnMut(&T) -> U) -> Vec<T> where T: Clone { let mut v = self.as_ref().to_vec(); v.sort_by_key(f); v }
@@ -64,6 +62,8 @@ pub trait MySlice<T> : AsRef<[T]> {
   fn sum(&self) -> T where T: Default + Clone + Add<Output = T> { let mut sum = T::default(); for x in self.citer() { sum = sum + x; }; sum }
   fn uniq(&self) -> Vec<T> where T: Clone + std::hash::Hash + Eq { self.citer().collect::<HashSet<_>>().into_iter().collect() }
   fn replace(&self, from: T, to: T) -> Vec<T> where T: Clone + PartialEq { let mut v = self.as_ref().to_vec(); for x in &mut v { if *x == from { *x = to.clone(); } }; v }
+  fn max_value(&self) -> T where T: Clone + Ord { self.citer().max().unwrap() }
+  fn min_value(&self) -> T where T: Clone + Ord { self.citer().max().unwrap() }
 }
 impl<T> MySlice<T> for [T] {}
 
