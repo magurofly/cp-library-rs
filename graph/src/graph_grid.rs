17fa43a1800094cb720faf83457c62fcea3b326b
magurofly <fishfishfish@hash.fyi>
2021-10-09 18:07:34 +0900

    うにゃ〜

--- graph/src/graph_grid.rs
+++ graph/src/graph_grid.rs
@@ -1,34 +1,50 @@
-use std::{marker::PhantomData, ops::{Deref, DerefMut, Index}};
+use std::{marker::PhantomData, ops::{Deref, DerefMut}};
 
 use super::*;
 
 pub type GridVecGraph<E> = GridGraph<Vec<Vec<(usize, E)>>, E>;
 
 pub struct GridGraph<G, E> {
-  grid: Vec<Vec<char>>,
+  rows: usize,
+  cols: usize,
   graph: G,
   phantom: PhantomData<E>,
 }
 
 impl<E, G: Graph<E>> GridGraph<G, E> {
+  pub fn new(rows: usize, cols: usize) -> Self where G: GraphMut<E> {
+    Self {
+      rows,
+      cols,
+      graph: G::new_graph(rows * cols),
+      phantom: PhantomData,
+    }
+  }
+
   pub fn rows(&self) -> usize {
-    self.grid.len()
+    self.rows
   }
 
   pub fn cols(&self) -> usize {
-    self.grid[0].len()
+    self.cols
   }
 
   pub fn id(&self, (i, j): (usize, usize)) -> usize {
     i * self.cols() + j
   }
 
-  pub fn each_vertex(&self, mut f: impl FnMut(usize, usize, char)) {
-    for i in 0 .. self.rows() {
-      for j in 0 .. self.cols() {
-        (f)(i, j, self.grid[i][j]);
+  pub fn each_vertex(&self, mut f: impl FnMut(usize, usize)) {
+    let r = self.rows();
+    let c = self.cols();
+    for i in 0 .. r {
+      for j in 0 .. c {
+        (f)(i, j);
       }
     }
+  }
+
+  pub fn vertices(&self) -> RectangleIterator {
+    RectangleIterator::new(self.rows(), self.cols())
   }
 }
 
@@ -55,22 +71,6 @@ impl<E, G: GraphMut<E>> GridGraph<G, E> {
   }
 }
 
-impl<E, G> Index<usize> for GridGraph<G, E> {
-  type Output = [char];
-
-  fn index(&self, idx: usize) -> &[char] {
-    &self.grid[idx]
-  }
-}
-
-impl<E, G> Index<(usize, usize)> for GridGraph<G, E> {
-  type Output = char;
-
-  fn index(&self, idx: (usize, usize)) -> &char {
-    &self.grid[idx.0][idx.1]
-  }
-}
-
 impl<E, G> Deref for GridGraph<G, E> {
   type Target = G;
 
@@ -85,10 +85,66 @@ impl<E, G> DerefMut for GridGraph<G, E> {
   }
 }
 
-impl<E, G: GraphMut<E>> From<Vec<Vec<char>>> for GridGraph<G, E> {
-  fn from(grid: Vec<Vec<char>>) -> Self {
+impl<E, G: GraphMut<E>> From<&Vec<Vec<char>>> for GridGraph<G, E> {
+  fn from(grid: &Vec<Vec<char>>) -> Self {
     let rows = grid.len();
     let cols = grid[0].len();
-    GridGraph { grid, graph: G::new_graph(rows * cols), phantom: PhantomData, }
+    Self::new(rows, cols)
   }
+}
+
+pub struct RectangleIterator {
+  h: usize, w: usize,
+  i: usize, j: usize,
+}
+impl RectangleIterator {
+  pub fn new(h: usize, w: usize) -> Self {
+    Self {
+      h, w,
+      i: 0, j: 0,
+    }
+  }
+}
+impl Iterator for RectangleIterator {
+  type Item = (usize, usize);
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.i < self.h {
+      let res = (self.i, self.j);
+      if self.j + 1 < self.w {
+        self.j += 1;
+      } else {
+        self.i += 1;
+        self.j = 0;
+      }
+      Some(res)
+    } else {
+      None
+    }
+  }
+}
+
+pub fn dbg_grid<G: IntoIterator<Item = H>, H: IntoIterator<Item = T>, T: std::fmt::Debug>(grid: G) {
+  eprintln!("{}", fmt_grid(grid));
+}
+
+pub fn fmt_grid<G: IntoIterator<Item = H>, H: IntoIterator<Item = T>, T: std::fmt::Debug>(grid: G) -> String {
+  let mut width = 0;
+  let formatted = grid.into_iter().map(|row| {
+    row.into_iter().map(|x| {
+      let s = format!("{:?}", x);
+      width = width.max(s.len());
+      s
+    }).collect::<Vec<_>>()
+  }).collect::<Vec<_>>();
+  let mut result = String::new();
+  for row in formatted {
+    for x in row {
+      let pad = width - x.len() + 1;
+      result.push_str(&x);
+      result.push_str(&" ".repeat(pad));
+    }
+    result.push('\n');
+  }
+  result
 }
